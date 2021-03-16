eb3eef6f664fbfa9afadd14fcc7739e67513ee05
Kye Burchard <kyeb@mit.edu>
2021-03-15 22:48:01 -0400

    lines static but better interface

--- components/animation.ts
+++ components/animation.ts
@@ -1,116 +1,86 @@
 interface Line {
+  m: number; // slope
+  b: number; // intercept
+  // deltaM: number;
+  // deltaB: number;
+}
+
+interface Points {
   x0: number;
   y0: number;
   x1: number;
   y1: number;
-  dirX: number;
-  dirY: number;
 }
 
 const numLines = 30;
 let positions: Line[] = [];
 const maxSpeed = 5;
+let w = 0;
+let h = 0;
 
-const getRandomLine = (w: number, h: number): Line => {
-  return {
-    x0: Math.floor(Math.random() * w),
-    y0: Math.floor(Math.random() * h),
-    x1: Math.floor(Math.random() * w),
-    y1: Math.floor(Math.random() * h),
-    // between -1 and 1
-    dirX: maxSpeed * (Math.random() - 0.5),
-    dirY: maxSpeed * (Math.random() - 0.5),
-  };
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+const getRandomInt = (min: number, max: number) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
 };
 
-const lineIsInteresting = (line: Line, w: number, h: number) => {
-  if (Math.abs(line.x0 - line.x1) < w * 0.05) return false;
-  if (Math.abs(line.y0 - line.y1) < h * 0.05) return false;
+const lineToPoints = (l: Line): Points => {
+  const x0 = 0;
+  const y0 = l.m * x0 + l.b;
+  const x1 = w;
+  const y1 = l.m * x1 + l.b;
+  return { x0, x1, y0, y1 };
+};
+
+const isVisible = (l: Line) => {
+  const { x0, y0, x1, y1 } = lineToPoints(l);
   return true;
 };
 
-export const initializeLines = (w: number, h: number) => {
+const getRandomLine = (i: number): Line => {
+  const k = numLines * 0.07; // no lines within 7% of vertical/horizontal
+
+  // theta between ~0 and ~pi/2
+  const theta = (((i + k) / (numLines + 2 * k)) * Math.PI) / 2;
+  const m = Math.tan(i % 2 === 0 ? theta : theta + Math.PI / 2);
+
+  // solve for b at each corner on the non y-axis side (at w, 0 and w, h)
+  const minIntercept = m > 0 ? -m * w : 0;
+  const maxIntercept = m > 0 ? h : h - m * w;
+  const b = getRandomInt(minIntercept, maxIntercept);
+  return { m, b };
+};
+
+const lineIsInteresting = (line: Line): boolean => {
+  return true;
+};
+
+export const updateDims = (width: number, height: number) => {
+  w = width;
+  h = height;
+};
+
+export const initializeLines = () => {
   positions = Array.from({ length: numLines }, (v, i) => {
     let line;
     do {
-      line = getRandomLine(w, h);
-      const direction = Math.floor(i % 6);
-      switch (direction) {
-        // ↗
-        case 0:
-          line.x0 = 0;
-          line.y1 = 0;
-          break;
-        // →
-        case 1:
-          line.x0 = 0;
-          line.x1 = w;
-          break;
-        // ↘
-        case 2:
-          line.x0 = 0;
-          line.y1 = h;
-          break;
-        // ↘
-        case 3:
-          line.y0 = 0;
-          line.x1 = w;
-          break;
-        // ↑
-        case 4:
-          line.y0 = 0;
-          line.y1 = h;
-          break;
-        // ↗
-        case 5:
-          line.x0 = w;
-          line.y1 = h;
-          break;
-      }
-    } while (!lineIsInteresting(line, w, h));
-
+      line = getRandomLine(i);
+    } while (!lineIsInteresting(line));
     return line;
   });
 };
 
 export const updateLines = (width: number, height: number) => {
-  const newX = (x: number, dir: number) => {
-    if (x <= 0) return 0;
-    else if (x >= width) return width;
-    else return x + dir;
-  };
-  const newY = (y: number, dir: number) => {
-    if (y <= 0) return 0;
-    else if (y >= height) return height;
-    else return y + dir;
-  };
-
-  const newXY = (l: Line) => ({
-    ...l,
-    x0: newX(l.x0, l.dirX),
-    y0: newY(l.y0, l.dirY),
-    x1: newX(l.x1, l.dirX),
-    y1: newY(l.y1, l.dirY),
-  });
-
-  const newPositions: Line[] = positions.map((l) => {
+  const newPositions: Line[] = positions.map((l, i) => {
     // off screen condition
-    const nextLine = newXY(newXY(l));
-    if (
-      (nextLine.x0 === 0 && nextLine.y0 == 0) ||
-      (nextLine.x0 === width && nextLine.y0 == 0) ||
-      (nextLine.x0 === 0 && nextLine.y0 == height) ||
-      (nextLine.x0 === width && nextLine.y0 == height) ||
-      (nextLine.x1 === 0 && nextLine.y1 == 0) ||
-      (nextLine.x1 === width && nextLine.y1 == 0) ||
-      (nextLine.x1 === 0 && nextLine.y1 == height) ||
-      (nextLine.x1 === width && nextLine.y1 == height)
-    ) {
-      l.dirX = -l.dirX;
-      l.dirY = -l.dirY;
-      return newXY(l);
+    let newLine;
+    if (!isVisible(l)) {
+      newLine = getRandomLine(i);
+    } else {
+      newLine = l;
     }
-    return newXY(l);
+    return newLine;
   });
 
   positions = newPositions;
@@ -121,7 +91,8 @@ export const drawLines = (ctx: CanvasRenderingContext2D) => {
   ctx.strokeStyle = "#ffffff";
   ctx.lineWidth = 0.5;
   ctx.beginPath();
-  positions.forEach((p) => {
+  positions.forEach((l) => {
+    const p = lineToPoints(l);
     ctx.moveTo(p.x0, p.y0);
     ctx.lineTo(p.x1, p.y1);
   });
