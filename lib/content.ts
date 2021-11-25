036981b03e89311ce3583cec341dcc037cdc4d76
Kye Burchard <kyeb@mit.edu>
2021-11-25 16:18:26 -0500

    Add prettier + nodenv

--- lib/content.ts
+++ lib/content.ts
@@ -1,5 +1,5 @@
-import { promises as fs } from 'fs';
-import path from 'path';
+import { promises as fs } from "fs";
+import path from "path";
 import { CONTENT_REPO } from "./constants";
 
 export interface ContentInfo {
@@ -44,22 +44,22 @@ const getGitHubContent = async (
 };
 
 const getLocalContent = async (dir: string): Promise<ContentInfo[]> => {
-  const contentDir = path.join(process.cwd(), 'content', dir);
+  const contentDir = path.join(process.cwd(), "content", dir);
   const contentPaths = await fs.readdir(contentDir);
 
   const contentPromises = contentPaths.map(async (fileName) => {
     const filePath = path.join(contentDir, fileName);
-    const fileContent = await fs.readFile(filePath, 'utf-8');
+    const fileContent = await fs.readFile(filePath, "utf-8");
     return { name: fileName, content: fileContent };
-  })
+  });
 
   return Promise.all(contentPromises);
-}
+};
 
 export const getProjects = async (): Promise<ContentInfo[]> => {
-  return getLocalContent('projects');
-}
+  return getLocalContent("projects");
+};
 
 export const getPosts = async () => {
-  return getLocalContent('posts');
-}
+  return getLocalContent("posts");
+};
