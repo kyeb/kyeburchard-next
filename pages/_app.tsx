dac0b990c58edc5a1135776684d821180b315981
Kye Burchard <kyeb@mit.edu>
2022-09-04 18:37:30 -0700

    Remove umami

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,14 +12,6 @@ const MyApp = (props: AppProps) => {
   const { Component, pageProps } = props;
   return (
     <>
-      {process.env.NODE_ENV === "production" && (
-        <script
-          async
-          defer
-          data-website-id="98d3a99c-b1bc-49ee-93b6-b825deb1ee0d"
-          src="https://umami.kyeburchard.com/umami.js"
-        ></script>
-      )}
       <Background />
       <Component {...pageProps} />
     </>
