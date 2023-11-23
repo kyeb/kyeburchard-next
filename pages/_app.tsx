e7143f461a5b7cb5409add270a8b491f29f3f7e9
Kye Burchard <kyeb@mit.edu>
2023-11-23 14:27:52 -0700

    switch to next optimized fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,20 +2,25 @@ import React from "react";
 import Background from "../components/Background";
 import "../styles/_globals.css";
 import "../styles/fontawesome-all.css";
+import { Lato } from "next/font/google"
+
+const lato = Lato({ weight: ["100", "300", "400", "700"], subsets: ["latin"], style: ["normal", "italic"] });
 
 interface AppProps {
   Component: any;
   pageProps: any;
 }
 
-const MyApp = (props: AppProps) => {
+const App = (props: AppProps) => {
   const { Component, pageProps } = props;
   return (
     <>
       <Background />
-      <Component {...pageProps} />
+      <main className={lato.className}>
+        <Component {...pageProps} />
+      </main>
     </>
   );
 };
 
-export default MyApp;
+export default App;
