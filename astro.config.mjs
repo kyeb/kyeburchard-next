2397cbdb042aff8f99aae3a2f7c088c016beea63
kyeb <hello@kyeb.com>
2026-01-14 09:56:38 -0800

    Move comment to line above

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import icon from 'astro-icon';
 
 export default defineConfig({
   site: 'https://kyeb.com',
-  trailingSlash: 'always', // Cloudflare Pages forces trailing slashes for static sites
+  // Cloudflare Pages forces trailing slashes for static sites
+  trailingSlash: 'always',
   output: 'static',
   integrations: [icon()],
 });
