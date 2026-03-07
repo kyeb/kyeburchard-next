fde7ea60fff49aedc72bce78ab1c8bafc0a36635
Kye Burchard <kyeb@mit.edu>
2026-03-06 18:19:09 -0800

    revamp consulting page

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,12 @@ import icon from 'astro-icon';
 export default defineConfig({
   site: 'https://kyeb.com',
   // Cloudflare Pages forces trailing slashes for static sites
-  trailingSlash: 'always',
+  trailingSlash: 'ignore',
   output: 'static',
   integrations: [icon()],
+  vite: {
+    server: {
+      allowedHosts: ['staging.kyeb.com'],
+    },
+  },
 });
